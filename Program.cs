429fe15cd40b5b7befe3389bedc945c546655fe4
saso949 <arukimakurou2@gmail.com>
2022-04-30 17:11:19 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 6 + 2;
+            int minus = 9 - 1;
+
+            sum += minus;
+            Console.WriteLine(sum);
+        }
+    }
+}
